34564439edd278019c50ec87cd2629077187ad81
poseidonwood <santosofebrikukuh@gmail.com>
2020-07-03 12:53:30 +0700

    Fixing in Kriteria

--- proses/hapus-kriteria.php
+++ proses/hapus-kriteria.php
@@ -3,7 +3,7 @@ include "../config/database.php";
 $id_kriteria = $_GET['id_kriteria'];
 $hapus = mysqli_query($koneksi, "delete from tbl_kriteria where id_kriteria ='$id_kriteria'");
 if ($hapus) {
-  echo "<script>window.location.href='../?page=analisa-kriteria&m=hapus-sukses';</script>";
+  echo "<script>window.location.href='../?page=kriteria&m=hapus-sukses';</script>";
 } else {
   echo "gagal hapus";
 }
--- proses/save-kriteria.php
+++ proses/save-kriteria.php
@@ -2,9 +2,7 @@
 include "../config/database.php";
 $nm_kriteria = $_POST['nm_kriteria'];
 
-$query = mysqli_query($koneksi,"insert into tbl_kriteria values ('','$nm_kriteria','')");
-if($query){
-	
-	echo "<script>window.location.href='".$domain."?page=kriteria&m=save';</script>";
+$query = mysqli_query($koneksi, "insert into tbl_kriteria values ('','$nm_kriteria')");
+if ($query) {
+	echo "<script>window.location.href='" . $domain . "?page=kriteria&m=save';</script>";
 }
-
